cbfbe2c484618ce5e0bfb397ab1c147e7fe99ad5
Amit Kumar <amit.kumar@magnushealth.com>
2025-08-12 18:06:15 +0530

    Incomplete codes.

---
+++ src/uber/DecipherStringWithNumbers.java
@@ -0,0 +1,19 @@
+package uber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecipherStringWithNumbers {
+
+    public static String decipherStringWithNumbers(String str) {
+
+        String ans = "";
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(decipherStringWithNumbers("3[a]2[bcd]"));
+    }
+}
