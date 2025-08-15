98b041cf90a7e034b2bd20857018e95325ad1693
Amit Kumar <amit.bit.it@gmail.com>
2025-08-15 12:04:58 +0530

    Class to print and count factorials of a given number.

---
+++ src/misc/Factorials.java
@@ -0,0 +1,93 @@
+package misc;
+
+import java.util.Scanner;
+
+public class Factorials {
+
+    public static long printAndCountFactors(long n, Speed speed){
+
+        switch (speed) {
+
+            case NORMAL:
+                return countFactors(n);
+            case FAST:
+                return countFactorsFast(n);
+            case FASTEST:
+                return countFactorsSuperfast(n);
+            default:
+                System.out.println( "Speed either not specified or not supported.");
+                return -1;
+        }
+    }
+
+    public static long countFactors(long n){
+
+        long ans = 0;
+        System.out.print("Factors: ");
+
+        for (long i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                System.out.print(i + "  ");
+                ans++;
+            }
+        }
+
+        System.out.println();
+        return ans;
+    }
+
+    public static long countFactorsFast(long n){
+
+        long ans = 2;
+        System.out.print("Factors: " + 1 + "  " + n + "  ");
+
+        for (long i = 2; i <= n / 2; i++) {
+            if(n % i == 0) {
+                System.out.print(i + "  ");
+                ans++;
+            }
+        }
+
+        System.out.println();
+        return ans;
+    }
+
+    public static long countFactorsSuperfast(long n){
+
+        int count = 0;
+        System.out.print("Factors: ");
+
+        for(long i = 1; i*i <= n; i++) {
+
+            if(n % i == 0) {
+                if(n / i == i) {
+                    System.out.print(i + "  ");
+                    count++;
+                } else {
+                    System.out.print(i + "  " + n/i + "  ");
+                    count += 2;
+                }
+            }
+        }
+
+        System.out.println();
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to find factors of it: ");
+        int n = scanner.nextInt();
+
+        for (Speed speed : Speed.getBasicOnes()) {
+
+            System.out.println("---------------------------------------");
+            System.out.println("Speed: " + speed);
+            System.out.println("---------------------------------------");
+            long startedAt = System.currentTimeMillis();
+            System.out.println("Total factors of " + n + " are: " + printAndCountFactors(n, speed));
+            System.out.println("Time taken: " +  ((System.currentTimeMillis() - startedAt) / 1000) + " seconds");
+        }
+    }
+}
