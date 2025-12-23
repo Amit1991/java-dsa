9dee7f2481e4dd4c301979f6a1e21b096614a31c
Amit Kumar <amit.bit.it@gmail.com>
2025-12-23 14:56:12 +0530

    Rename MaxMinOfArray to ArrayOpsUsingRecursion and add methods to find first and last index of a target element using recursion

--- src/misc/recursion/ArrayOpsUsingRecursion.java
+++ src/misc/recursion/ArrayOpsUsingRecursion.java
@@ -2,7 +2,7 @@ package misc.recursion;
 
 import arrays.ArrayUtils;
 
-public class MaxMinOfArray {
+public class ArrayOpsUsingRecursion {
 
     /**
     * Recursively finds the maximum element in an integer array.
@@ -26,10 +26,36 @@ public class MaxMinOfArray {
         return minOfArray(arr, curr+1, Math.min(arr[curr], min));
     }
 
+    /**
+     * Recursively finds the first index of a target element
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int firstIndex(int[] arr, int curr, int target) {
+
+        if(arr[curr] == target) return curr;
+        if(curr == arr.length -1) return arr[curr] == target ? curr : -1;
+        return firstIndex(arr, curr+1, target);
+    }
+
+    /**
+     * Recursively finds the last index of a target element
+     * TC: O(n)
+     * SC: O(n)
+     */
+    public static int lastIndex(int[] arr, int curr, int target) {
+
+        if(arr[curr] == target) return curr;
+        if(curr == 0) return arr[curr] == target ? curr : -1;
+        return lastIndex(arr, curr-1, target);
+    }
+
     public static void main(String[] args) {
 
         int[] sampleArr = ArrayUtils.getIntegerArrayInput();
         System.out.println(maxOfArray(sampleArr,0, Integer.MIN_VALUE));
         System.out.println(minOfArray(sampleArr,0, Integer.MAX_VALUE));
+        System.out.println(firstIndex(sampleArr, 0, 1));
+        System.out.println(lastIndex(sampleArr, sampleArr.length-1, 1));
     }
 }
